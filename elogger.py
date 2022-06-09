bcbde0740721a339abf186edfb584f84a9be34f2
root <root@HAProxy-server1.csg2ojf14x1ufinqtxn5hek11f.ax.internal.cloudapp.net>
2022-06-09 14:42:14 +0000

    renamed variables so the code will be easier to read

--- elogger.py
+++ elogger.py
@@ -17,14 +17,14 @@ def write(event_string):
     # TODO: add value check
 
     logger.info(event_string, extra={"http.request.method": "get", "UUID": json_UUID})
-    a_file = open("/home/tzur/elvis.json", "r")
-    a_json = json.load(a_file)
-    a_file.close()
+    log_content = open("/home/tzur/elvis.json", "r")
+    log_content_json = json.load(log_content)
+    log_content.close()
 
     os.remove("/home/tzur/elvis.json")
 
     doc_UUID = uuid.uuid4()
-    resp = requests.put(url=f"http://13.81.211.207:9200/{event_string}/_doc/{doc_UUID}", json=a_json,
+    resp = requests.put(url=f"http://13.81.211.207:9200/{event_string}/_doc/{doc_UUID}", json=log_content_json,
                         headers={'Content-Type': 'application/json'})
     #print(resp.json())
 
